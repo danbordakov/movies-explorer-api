b2b8e2709b6f1a3fd38bae098935a6109a34f14a
danbordakov <danbordakov@yandex.ru>
2024-04-16 17:50:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,23 +11,28 @@ const { requestLogger, errorLogger } = require("./middlewares/logger");
 const limiter = require("./middlewares/rateLimit");
 
 const { PORT = 3000, DB_PATH = "mongodb://127.0.0.1:27017/bitfilmsdb" } = process.env;
+const options = {
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://diploma.bordakov.nomoredomainswork.ru",
+    "http://diploma.bordakov.nomoredomainswork.ru",
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 const app = express();
 
+app.use(cors(options));
 app.use(helmet());
 app.use(limiter);
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://diploma.bordakov.nomoredomainswork.ru",
-    ],
-    credentials: true,
-  })
-);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
